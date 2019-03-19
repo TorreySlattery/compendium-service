b67263d1f73c1db053daf825de86bcde61eb4d2d
Torrey Slattery <torrey.slattery@gmail.com>
2019-03-19 12:49:57 -0700

    Add some Spell admin stuff and a Thing migration

--- lookups/admin.py
+++ lookups/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from lookups import models
+
+
+class SpellAdmin(admin.ModelAdmin):
+    model = models.Spell
+
+    list_display = ('name', 'short_description',)
+    search_fields = ('name', 'short_description',)
+
+
+admin.site.register(models.Spell, SpellAdmin)
--- lookups/models.py
+++ lookups/models.py
@@ -6,11 +6,27 @@ class Thing(models.Model):
         abstract = True
 
     name = models.CharField(
+        unique=True,
         max_length=255,
         null=False,
         blank=False,
         default=None,
         help_text="The abstract parent class for all reference lookup types"
+    )
+
+    short_description = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        default=None,
+        help_text="A roughly one-breath sentence that describes the thing"
+    )
+
+    full_description = models.TextField(
+        null=True,
+        blank=True,
+        default=None,
+        help_text="The full description of what the thing is"
     )
 
     def __str__(self):
