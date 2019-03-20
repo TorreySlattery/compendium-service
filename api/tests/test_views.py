893a98a5ab11647e45d7ee681b22c555829c064e
Torrey Slattery <torrey.slattery@gmail.com>
2019-03-20 11:25:45 -0700

    Add more tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -27,3 +27,20 @@ class SpellGetEndpointTestCase(APITestCase):
         """Should return a list of existing items"""
         response = self.client.get(self.list_url)
         assert len(response.data) == 5
+
+    def test_detail_exists(self):
+        """Should return a 200 on the retrieve endpoint"""
+        pk = models.Spell.objects.first().pk
+        response = self.client.get(self._detail_url(pk))
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_detail_returns_item(self):
+        """Should return a single item on the retrieve endpoint"""
+        pk = models.Spell.objects.first().pk
+        response = self.client.get(self._detail_url(pk))
+        assert response.data['id'] == pk
+
+    def test_detail_404(self):
+        """Should return a 404 on the retrieve endpoint if the pk doesn't exist"""
+        response = self.client.get(self._detail_url('9000'))
+        assert response.status_code == status.HTTP_404_NOT_FOUND
