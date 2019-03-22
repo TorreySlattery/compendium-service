5fa10b068c0ed452bdd6b06ef1e950a34d5d482f
Torrey Slattery <torrey.slattery@gmail.com>
2019-03-22 14:08:41 -0700

    Update Spell admin a bit

--- lookups/admin.py
+++ lookups/admin.py
@@ -6,10 +6,13 @@ from lookups import models
 class ThingAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description',)
     search_fields = ('name', 'short_description',)
+    ordering = ('name',)
 
 
 class SpellAdmin(ThingAdmin):
     model = models.Spell
+    list_display = ('name', 'is_concentration', 'is_ritual', 'short_description')
+    list_filter = ('is_concentration', 'is_ritual',)
 
 
 class ItemAdmin(ThingAdmin):
